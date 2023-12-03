950eed41de921107bb51fdba3cdaad63dfcd52d7
o-l-a <kaczynska256@onet.pl>
2023-12-03 13:13:06 +0100

    Changed system navigation bar color

--- app/src/main/java/com/example/myinputlog/MainActivity.kt
+++ app/src/main/java/com/example/myinputlog/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -17,7 +19,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyInputLogTheme {
-                StatusBarColor(color = MaterialTheme.colorScheme.background)
+                SystemBarColors(
+                    statusBarColor = MaterialTheme.colorScheme.background,
+                    navigationBarColor = MaterialTheme.colorScheme.surfaceColorAtElevation(NavigationBarDefaults.Elevation)
+                )
                 MyInputLogApp()
             }
         }
@@ -25,9 +30,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun StatusBarColor(color: Color) {
+fun SystemBarColors(statusBarColor: Color, navigationBarColor: Color) {
     val systemUiController = rememberSystemUiController()
     SideEffect {
-        systemUiController.setStatusBarColor(color)
+        systemUiController.setStatusBarColor(statusBarColor)
+        systemUiController.setNavigationBarColor(navigationBarColor)
     }
 }
